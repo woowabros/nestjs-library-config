31babff88643e8bb0891b3a611285525246c2311
jiho.park <jiho@woowahan.com>
2023-04-02 09:22:59 +0900

    feat: configModuleOptions

--- spec/dotenv-source-provider/dotenv-source-provider.spec.ts
+++ spec/dotenv-source-provider/dotenv-source-provider.spec.ts
@@ -1,17 +1,52 @@
 import path from 'path';
 
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { ConfigService } from './config.service';
+import { ConfigModule } from '../../src';
 import { DotenvSourceProvider } from '../../src/lib/provider/dotenv-source-provider';
 
-describe('dotenv provider의 동작을 검증합니다', () => {
-    it('파일이 없을때 error를 throw 하여야 합니다', () => {
-        expect(() => new DotenvSourceProvider({ path: './not-found-file.env' }).export()).toThrowError();
+describe('DotenvSourceProvider', () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forFeature(ConfigService, {
+                    sourceProvider: new DotenvSourceProvider({ path: path.join(__dirname, 'test.env') }),
+                }),
+            ],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
     });
 
-    it('환경 변수 파일을 로드할 수 있습니다', () => {
-        const absPath = path.join(__dirname, 'test.env');
-        expect(new DotenvSourceProvider({ path: absPath }).export()).toEqual({
-            LISTENING_PORT: '9999',
-            REDIS_URL: 'hello',
+    afterEach(async () => {
+        await app?.close();
+    });
+
+    it('should configure ConfigService from env file', () => {
+        const service = app.get<ConfigService>(ConfigService);
+        expect(service).toEqual({
+            port: 9999,
+            url: 'hello',
+            host: 'default',
+        });
+    });
+
+    describe('features of DotenvSourceProvider', () => {
+        it('should load environment file', () => {
+            const absPath = path.join(__dirname, 'test.env');
+            expect(new DotenvSourceProvider({ path: absPath }).export()).toEqual({
+                LISTENING_PORT: '9999',
+                URL: 'hello',
+            });
+        });
+
+        it('should be through error, when file is not exist', () => {
+            expect(() => new DotenvSourceProvider({ path: './not-found-file.env' }).export()).toThrowError();
         });
     });
 });
