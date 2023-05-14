76796500b4617dcc07ed2e0b3b2c67cd58f210cb
thilllon <jakelee@woowahan.com>
2023-05-14 17:37:37 +0900

    style: format yaml (#48)
    * style: format yaml
    
    * feat: add yaml files to precommit hook target

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    '*.(md|yaml|yml)': ['prettier --write'],
     '*.+(ts|js|json)': ['eslint --fix', 'prettier --write'],
     '*.+(ts)': [() => 'tsc -p tsconfig.lib.json --noEmit', () => 'tsc -p tsconfig.spec.json --noEmit'],
 };
