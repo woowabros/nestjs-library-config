e2f4202be1313b5afe446cfcfee5ede4363958a9
Youngho Kim <jaden.kim5179@gmail.com>
2024-08-15 22:00:51 +0900

    feat: export config source providers (#415)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 export * from './lib/config.module';
 export * from './lib/abstract/abstract-config.service';
 
+export * from './lib/abstract/abstract-config-source-provider';
+export * from './lib/provider/dotenv-source-provider';
+export * from './lib/provider/process-env-source-provider';
+
 export * from './lib/type-transformer';
 export * from './lib/config-scanner';
